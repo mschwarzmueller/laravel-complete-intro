c23bc0609dae69b0caba7f2220abc8995472376c
Maximilian Schwarzmüller <mblacky0@gmail.com>
2015-12-11 00:09:02 +0100

    Completet View Tutorial Code

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,9 @@
+<footer>
+    <nav>
+        <ul>
+            <li><a href="#">Legal</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="#">About</a></li>
+        </ul>
+    </nav>
+</footer>
