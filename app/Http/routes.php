aa69bf8519ff8fecb7f93fe9bcfe8f288504c468
Maximilian Schwarzmüller <mblacky0@gmail.com>
2015-12-30 08:57:57 +0100

    Add auth middleware and route grouping

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -46,89 +46,91 @@ Route::post('/contact/sendmail', [
 ]);
 
 /* Backend routes */
-Route::get('/admin', [
-    'uses' => 'AdminController@getIndex',
-    'as' => 'admin.index'
-]);
-
 Route::get('/admin/login', [
     'uses' => 'AdminController@getLogin',
     'as' => 'admin.login'
 ]);
-
 Route::post('/admin/login', [
     'uses' => 'AdminController@postLogin',
     'as' => 'admin.login'
 ]);
+Route::group([
+    'prefix' => '/admin',
+    'middleware' => 'auth'
+], function () {
+    Route::get('/', [
+        'uses' => 'AdminController@getIndex',
+        'as' => 'admin.index'
+    ]);
+    Route::get('/logout', [
+        'uses' => 'AdminController@getLogout',
+        'as' => 'admin.logout'
+    ]);
 
-Route::get('/admin/logout', [
-    'uses' => 'AdminController@getLogout',
-    'as' => 'admin.logout'
-]);
+    /* Blog routes */
+    Route::get('/blog/posts', [
+        'uses' => 'PostController@getPostIndex',
+        'as' => 'admin.blog.index'
+    ]);
 
-/* Blog routes */
-Route::get('/admin/blog/posts', [
-    'uses' => 'PostController@getPostIndex',
-    'as' => 'admin.blog.index'
-]);
+    Route::get('/blog/categories', [
+        'uses' => 'CategoryController@getCategoryIndex',
+        'as' => 'admin.blog.categories'
+    ]);
 
-Route::get('/admin/blog/categories', [
-    'uses' => 'CategoryController@getCategoryIndex',
-    'as' => 'admin.blog.categories'
-]);
+    Route::get('/blog/post/{post_id}&{end}', [
+        'uses' => 'PostController@getSinglePost',
+        'as' => 'admin.blog.post'
+    ]);
 
-Route::get('/admin/blog/post/{post_id}&{end}', [
-    'uses' => 'PostController@getSinglePost',
-    'as' => 'admin.blog.post'
-]);
+    Route::get('/blog/posts/create', [
+        'uses' => 'PostController@getCreatePost',
+        'as' => 'admin.blog.create_post'
+    ]);
 
-Route::get('/admin/blog/posts/create', [
-    'uses' => 'PostController@getCreatePost',
-    'as' => 'admin.blog.create_post'
-]);
+    Route::get('/blog/post/{post_id}/edit', [
+        'uses' => 'PostController@getUpdatePost',
+        'as' => 'admin.blog.post.edit'
+    ]);
 
-Route::get('/admin/blog/post/{post_id}/edit', [
-    'uses' => 'PostController@getUpdatePost',
-    'as' => 'admin.blog.post.edit'
-]);
+    Route::post('/blog/post/create', [
+        'uses' => 'PostController@postCreatePost',
+        'as' => 'admin.blog.post.create'
+    ]);
 
-Route::post('/admin/blog/post/create', [
-    'uses' => 'PostController@postCreatePost',
-    'as' => 'admin.blog.post.create'
-]);
+    Route::post('/blog/category/create', [
+        'uses' => 'CategoryController@postCreateCategory',
+        'as' => 'admin.blog.category.create'
+    ]);
 
-Route::post('/admin/blog/category/create', [
-    'uses' => 'CategoryController@postCreateCategory',
-    'as' => 'admin.blog.category.create'
-]);
+    Route::post('/blog/posts/update', [
+        'uses' => 'PostController@postUpdatePost',
+        'as' => 'admin.blog.post.update'
+    ]);
 
-Route::post('/admin/blog/posts/update', [
-    'uses' => 'PostController@postUpdatePost',
-    'as' => 'admin.blog.post.update'
-]);
+    Route::post('/blog/categories/update', [
+        'uses' => 'CategoryController@postUpdateCategory',
+        'as' => 'admin.blog.category.update'
+    ]);
 
-Route::post('/admin/blog/categories/update', [
-    'uses' => 'CategoryController@postUpdateCategory',
-    'as' => 'admin.blog.category.update'
-]);
+    Route::get('/blog/post/{post_id}/delete', [
+        'uses' => 'PostController@getDeletePost',
+        'as' => 'admin.blog.post.delete'
+    ]);
 
-Route::get('/admin/blog/post/{post_id}/delete', [
-    'uses' => 'PostController@getDeletePost',
-    'as' => 'admin.blog.post.delete'
-]);
+    Route::get('/blog/category/{category_id}/delete', [
+        'uses' => 'CategoryController@getDeleteCategory',
+        'as' => 'admin.blog.category.delete'
+    ]);
 
-Route::get('/admin/blog/category/{category_id}/delete', [
-    'uses' => 'CategoryController@getDeleteCategory',
-    'as' => 'admin.blog.category.delete'
-]);
+    /* Other routes */
+    Route::get('/contact/messages', [
+        'uses' => 'ContactMessageController@getContactMessageIndex',
+        'as' => 'admin.contact.index'
+    ]);
 
-/* Other routes */
-Route::get('/admin/contact/messages', [
-    'uses' => 'ContactMessageController@getContactMessageIndex',
-    'as' => 'admin.contact.index'
-]);
-
-Route::get('/admin/contact/message/{message_id}/delete', [
-    'uses' => 'ContactMessageController@getDeleteMessage',
-    'as' => 'admin.contact.delete'
-]);
+    Route::get('/contact/message/{message_id}/delete', [
+        'uses' => 'ContactMessageController@getDeleteMessage',
+        'as' => 'admin.contact.delete'
+    ]);
+});
